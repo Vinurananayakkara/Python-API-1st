71a933145130c0f6bb3dc2d67e77b9b5cd3b8e9a
Vinura Nanayakkara <vinurananayakkara24@gmail.com>
2025-03-14 21:13:12 +0530

    complete Flask CRUD operation

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 
 app =Flask(__name__)
 
@@ -13,12 +14,90 @@ class UserModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), nullable=False)
 
-    def __repr__(self):
+    def __repr__(self): #represent 
         return f'User(name = {self.name}, email={self.email})'
+
+#validate requests   
+user_args = reqparse.RequestParser()
+user_args.add_argument('name',type=str,required=True,help="Name cannot be blank")
+user_args.add_argument('email',type=str,required=True,help="Email cannot be blank")
+
+#api kiyl app eke instance ekak
+api = Api(app) 
+
+#json format ekt res gnna - userFields
+userFields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'email':fields.String,
+}
+
+
+class Users(Resource):
+
+    #get api
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users 
+    
+    #post api
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args() #arguments filter krnna 
+        user = UserModel(name=args["name"] , email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users,201
+
+class User(Resource):
+
+    @marshal_with(userFields)
+    def get(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"User not found")
+        return user
+    
+    @marshal_with(userFields)
+    def patch(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        args = user_args.parse_args() 
+        if not user:
+            abort(404,"User not found")
+        
+        user.name = args["name"]
+        user.email = args["email"]
+        db.session.commit()
+        users = UserModel.query.all()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"User not found")
+        
+        db.session.delete(user)
+        db.session.commit()
+
+        users = UserModel.query.all()
+        return user
+    
+    
+    
+    
+
+api.add_resource(Users,'/api/users/')
+api.add_resource(User,'/api/users/<int:id>')
+
+
+
 
 @app.route('/') 
 def home():
-    return '<h1>Hello World</h1>'
+    return '<h1>Flask Rest APIs</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
